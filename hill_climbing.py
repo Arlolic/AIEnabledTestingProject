5575c65451c3030310690f5651c4695ee3c7da19
victorzyang <victor.yang2016@gmail.com>
2023-12-14 17:08:28 -0500

    Added pseudocode for hill climbing algorithm

--- hill_climbing.py
+++ hill_climbing.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 #For each MDG, get the number of nodes and edges
 num_of_nodes = []
@@ -42,6 +43,30 @@ print(num_of_edges_list) #The number of edges for the 'bunch' (364) and the 'bun
 
 #Hill climbing algorithm takes Module Dependency Graphs as input
 
-#Set climb = True
+def hill_climb(weighted_connections, modules, num_of_modules):
+    # Perform initial set of hill climbs
 
-# MQ fitness function, where MQ is sum of all the Modularization Factors (MF) which is the ratio of inner to outer edges within each module or group
+    #Assign each module to a building block (Note: A building block contains one or more modules fixed to be in a particular cluster, if and only if all the selected 
+    # initial hill climbs agree that these modules were to be located within the same cluster)
+
+    n = random.randint(1, num_of_modules)
+
+    #Building block is going to be a list (or can it be a set?)
+    list_of_building_blocks = [[] for x in range(n)] #Followed this site here: https://stackoverflow.com/questions/13520876/how-can-i-make-multiple-empty-lists-in-python
+
+    for x in range(num_of_modules):
+        #Assign each module to a building block
+
+
+    #Larger edge weights indicate more dependency between modules and an increase in the likely hood that they should be placed in the same cluster
+
+    #Set climb = True
+
+        # MQ fitness function, where MQ is sum of all the Modularization Factors (MF) which is the ratio of inner to outer edges within each module or group (Evaluate the
+        # fitness of the clustering)
+
+        # Search ends when none of the nearest neighbours from a clustering can yield a better MQ value
+
+    # Need to identify common features of each solution in best hill climb to form building blocks for subsequent hill climb
+
+    # Perform final set of hill climbs, identical to that of initial set of hill climbs
